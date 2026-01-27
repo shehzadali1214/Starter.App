550f347942588a35d83043fdebf22575d3a696ce
shehzadali1214 <shehzad.nazar@codeavour.com>
2026-01-27 19:03:55 +0500

    update apk

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -16,7 +16,6 @@ export default function HomeScreen() {
 
     );
 }
-
 const styles = StyleSheet.create({
     container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
     title: { fontSize: 28, fontWeight: '700' },
--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-
 export default function SettingsScreen() {
   return (
     <View style={styles.container} className='bg-orange-500'>
@@ -9,7 +8,6 @@ export default function SettingsScreen() {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
   title: {fontSize: 28, fontWeight: '700'},
